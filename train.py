f0d0e0cc001241e387c94afdaf12575baefb060a
artrocha <arthurhenrique.rocha@gmail.com>
2022-06-22 12:56:02 -0300

    minha nooooossa

--- train.py
+++ train.py
@@ -1,4 +1,5 @@
 from pickle import TRUE
+from pkgutil import get_loader
 from turtle import width
 import torch
 import albumentations as A
@@ -7,13 +8,13 @@ from tqdm import tqdm
 import torch.nn as nn
 import torch.optim as optim
 from model import UNET
-# from utils import (
-#     load_checkpoint,
-#     save_checkpoint,
-#     get_loaders,
-#     check_accuracy,
-#     save_predictions_as_img,
-# )
+from utils import (
+    load_checkpoint,
+    save_checkpoint,
+    get_loaders,
+    check_accuracy,
+    save_predictions_as_img,
+)
 
 #HYPERPARAMETERS ETC
 LEARNING_RATE = 1E-4
@@ -70,6 +71,61 @@ def main():
         ),
         ToTensorV2(),
     ])
+
+    val_transforms = A.Compose(
+        [A.Resize(height=IMAGE_HEIGHT,width=IMAGE_WIDTH),
+        A.Normalize(
+            mean=[0.0,0.0,0.0],
+            std = [1.0,1.0,1.0],
+            max_pixel_value=255.0,
+        ),
+        ToTensorV2(),
+        ],
+    )
+
+    model =UNET(in_channels=3,out_channels=1).to(DEVICE)
+    loss_fn = nn.BCEWithLogitsLoss() #cross entropy loss
+    optimizer = optim.Adam(model.parameters(), lr=LEARNING_RATE)
+
+    train_loader, val_loader = get_loaders(
+        TRAIN_IMG_DIR,
+        TRAIN_MASK_DIR,
+        VAL_IMG_DIR,
+        VAL_MASK_DIR,
+        BATCH_SIZE,
+        train_transform,
+        val_transforms,
+        NUM_WORKERS,
+        PIN_MEMORY
+    )
+
+    if LOAD_MODEL:
+        load_checkpoint(torch.load('my_checkpoint.pth.tar'),model)
+
+    check_accuracy(val_loader,model,device=DEVICE)
+
+    scaler = torch.cuda.amp.GradScaler()
+
+    for epoch in range(NUM_EPOCHS):
+        train_fn(train_loader,model,optimizer,loss_fn,scaler)
+
+        #salve model 
+        checkpoint = {
+            'state_dict': model.state_dict(),
+            'optimizer': optimizer.state_dict(),
+        }
+        save_checkpoint(checkpoint)
+        
+        
+        #chack accuracy
+        check_accuracy(val_loader,model,device=DEVICE)
+
+
+        # print some examples to a folder
+        save_predictions_as_img(
+            val_loader, model, folder='saved_images/',device=DEVICE
+        )
+
 
 if __name__ =='__main__':
     main()
